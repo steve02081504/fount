003d9faf5379633ce2f6cb98b3579648f2abe81a
K3K1337 <A1651155602@GMAIL.COM>
2024-10-04 15:56:30 +0800

    Add existing file

--- src/server/server.mjs
+++ src/server/server.mjs
@@ -33,11 +33,14 @@ if (!config.secretKey) {
 }
 
 export async function init() {
+	var open = require('open')
 	registerEndpoints(app)
 	app.use(express.static(__dirname + '/src/public'))
 	const { port } = config
 	app.listen(port, () => {
 		console.log(`服务器运行在 http://localhost:${port}`)
+	open(`http://localhost:${port}`)
 	})
+
 }
 
