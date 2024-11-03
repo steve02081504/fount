7f6171047dc7a58946fbf37d4f34d9bb71666000
steve02081504 <steve02081504@EDEN.fukwold>
2024-11-03 14:09:15 +0800

    update

--- path/fount.bat
+++ path/fount.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 set fount_dir=%~dp0/..
 if not exist node_modules (
-	npm install --prefix %fount_dir% --no-optional
+	npm install --prefix %fount_dir% --omit=optional
 )
 npm run --prefix %fount_dir% start %*
 @echo on
