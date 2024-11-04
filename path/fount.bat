0bdfdf67331c04d1a2247ef05342e69d051562f2
steve02081504 <steve02081504@EDEN.fukwold>
2024-11-05 00:29:10 +0800

    fixes

--- path/fount.bat
+++ path/fount.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 set fount_dir=%~dp0/..
 if not exist node_modules (
-	deno install --allow-all "--entrypoint=%fount_dir%/src/server/index.mjs" --node-modules-dir=auto
+	deno install --allow-scripts --allow-all "--entrypoint=%fount_dir%/src/server/index.mjs" --node-modules-dir=auto
 )
-deno run --allow-all "%fount_dir%/src/server/index.mjs" %*
+deno run --allow-scripts --allow-all "%fount_dir%/src/server/index.mjs" %*
 @echo on
--- path/fount.sh
+++ path/fount.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 FOUNT_DIR=$(dirname "$SCRIPT_DIR")
 
 if [ ! -d "$FOUNT_DIR/node_modules" ]; then
-	deno install --allow-all "--entrypoint=$FOUNT_DIR/src/server/index.mjs" --node-modules-dir=auto
+	deno install --allow-scripts --allow-all "--entrypoint=$FOUNT_DIR/src/server/index.mjs" --node-modules-dir=auto
 fi
 
-deno run --allow-all "$FOUNT_DIR/src/server/index.mjs" $@
+deno run --allow-scripts --allow-all "$FOUNT_DIR/src/server/index.mjs" $@
