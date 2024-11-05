0f0de0c7ac77100e9a89fbbc268ce4df2fe2c3ca
steve02081504 <steve02081504@EDEN.fukwold>
2024-11-05 23:03:27 +0800

    updates

---
+++ src/decl/charTemplateAPI.ts
@@ -0,0 +1,25 @@
+import { locale_t } from "./basedefs";
+
+export class charTemplateAPI_t {
+	info: Record<locale_t, {
+		name: string;
+		avatar: string;
+		description: string;
+		description_markdown: string;
+		version: string;
+		author: string;
+		homepage: string;
+		issuepage: string;
+		tags: string[];
+	}>;
+	// calls only on char install, and if fail, all file under this char's folder will be deleted
+	Init: () => void;
+	// calls on every char start, pop a message if fail
+	Load: () => void;
+	// calls on every char unload
+	Unload: (reason: string) => void;
+	// calls on char uninstall
+	Uninstall: (reason: string, from: string) => void;
+
+	ImportChar: (username: string, chardata: Buffer) => Promise<void>;
+}
--- src/public/scripts/chars.mjs
+++ src/public/scripts/chars.mjs
@@ -1,9 +1,9 @@
 export async function getCharList() {
-	const response = await fetch('/api/charlist')
+	const response = await fetch('/api/getlist/chars')
 	return response.json()
 }
 export async function getCharDetails(charname) {
-	const response = await fetch('/api/chardetails?charname=' + charname, {
+	const response = await fetch('/api/getdetails/chars?name=' + charname, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
