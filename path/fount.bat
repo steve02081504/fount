b739fbfe36dc642023025cae080220ea1c53708b
steve02081504 <steve02081504@EDEN.fukwold>
2024-11-10 18:38:58 +0800

    update

--- path/fount.bat
+++ path/fount.bat
@@ -1,8 +1,4 @@
 @echo off
 setlocal enabledelayedexpansion
-set fount_dir=%~dp0/..
-if not exist node_modules (
-	deno install --allow-scripts --allow-all "--entrypoint=%fount_dir%/src/server/index.mjs" --node-modules-dir=auto
-)
-deno run --allow-scripts --allow-all "%fount_dir%/src/server/index.mjs" %*
+powershell.exe -noprofile -executionpolicy bypass -file "%~dp0fount.ps1" %*
 @echo on
--- path/fount.ps1
+++ path/fount.ps1
@@ -1,7 +1,17 @@
 $FOUNT_DIR = Split-Path -Parent $PSScriptRoot
+$ErrorCount = $Error.Count
+
+if (!(Get-Command deno -ErrorAction SilentlyContinue)) {
+	Invoke-RestMethod https://deno.land/install.ps1 | Invoke-Expression
+}
 
 if (!(Test-Path -Path "$FOUNT_DIR/node_modules")) {
 	deno install --allow-scripts --allow-all "--entrypoint=$FOUNT_DIR/src/server/index.mjs" --node-modules-dir=auto
 }
 
 deno run --allow-scripts --allow-all "$FOUNT_DIR/src/server/index.mjs" @args
+
+if ($ErrorCount -ne $Error.Count -or $LASTEXITCODE -ne 0) {
+	Pause
+	exit $LASTEXITCODE
+}
