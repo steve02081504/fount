55ff9631c510de609aba42fe7dd334cb9966e11c
steve02081504 <steve02081504@EDEN.fukwold>
2024-11-15 23:49:51 +0800

    update

--- path/fount.bat
+++ path/fount.bat
@@ -1,4 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
 powershell.exe -noprofile -executionpolicy bypass -file "%~dp0fount.ps1" %*
+exit %ERRORLEVEL%
 @echo on
--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 @echo off
 call "%~dp0/path/fount.bat" %*
+exit %ERRORLEVEL%
 @echo on
