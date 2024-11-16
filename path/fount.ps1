6baba697449150d57416d8ebc3731a41d8c391dc
steve02081504 <steve02081504@EDEN.fukwold>
2024-11-16 14:45:33 +0800

    run script update

--- path/fount.ps1
+++ path/fount.ps1
@@ -1,12 +1,30 @@
 $FOUNT_DIR = Split-Path -Parent $PSScriptRoot
 $ErrorCount = $Error.Count
 
+if (!(Get-Command git -ErrorAction SilentlyContinue)) {
+	if (!(Get-Command winget -ErrorAction SilentlyContinue)) {
+		Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+	}
+	winget install --id Git.Git -e --source winget
+}
+if (!(Test-Path -Path "$FOUNT_DIR/.git")) {
+	Remove-Item -Path "$FOUNT_DIR/.git-clone" -Recurse -Force -ErrorAction SilentlyContinue
+	New-Item -ItemType Directory -Path "$FOUNT_DIR/.git-clone"
+	git clone https://github.com/steve02081504/fount.git "$FOUNT_DIR/.git-clone" --no-checkout --depth 1
+	Move-Item -Path "$FOUNT_DIR/.git-clone/.git" -Destination "$FOUNT_DIR/.git"
+	Remove-Item -Path "$FOUNT_DIR/.git-clone" -Recurse -Force
+	git -C "$FOUNT_DIR" fetch origin
+	git -C "$FOUNT_DIR" reset --hard origin/master
+	git -C "$FOUNT_DIR" checkout origin/master
+}
+git -C "$FOUNT_DIR" pull
+
 if (!(Get-Command deno -ErrorAction SilentlyContinue)) {
 	Invoke-RestMethod https://deno.land/install.ps1 | Invoke-Expression
 }
 
 if (!(Test-Path -Path "$FOUNT_DIR/node_modules")) {
-	deno install --allow-scripts --allow-all "--entrypoint=$FOUNT_DIR/src/server/index.mjs" --node-modules-dir=auto
+	deno install --allow-scripts --allow-all --node-modules-dir=auto --entrypoint "$FOUNT_DIR/src/server/index.mjs"
 }
 
 deno run --allow-scripts --allow-all "$FOUNT_DIR/src/server/index.mjs" @args
--- path/fount.sh
+++ path/fount.sh
@@ -3,8 +3,42 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 FOUNT_DIR=$(dirname "$SCRIPT_DIR")
 
+if ! command -v git &> /dev/null; then
+	if command -v apt-get &> /dev/null; then
+		sudo apt-get update
+		sudo apt-get install -y git
+	elif command -v brew &> /dev/null; then
+		brew install git
+	elif command -v pacman &> /dev/null; then
+		sudo pacman -Syy
+		sudo pacman -S --needed git
+	elif command -v dnf &> /dev/null; then
+		sudo dnf install -y git
+	elif command -v zypper &> /dev/null; then
+		sudo zypper install -y git
+	else
+		echo "git could not be found"
+		exit 1
+	fi
+fi
+if [ ! -d "$FOUNT_DIR/.git" ]; then
+	rm -rf "$FOUNT_DIR/.git-clone"
+	mkdir -p "$FOUNT_DIR/.git-clone"
+	git clone https://github.com/steve02081504/fount.git "$FOUNT_DIR/.git-clone" --no-checkout --depth 1
+	mv "$FOUNT_DIR/.git-clone/.git" "$FOUNT_DIR/.git"
+	rm -rf "$FOUNT_DIR/.git-clone"
+	git -C "$FOUNT_DIR" fetch origin
+	git -C "$FOUNT_DIR" reset --hard origin/master
+	git -C "$FOUNT_DIR" checkout origin/master
+fi
+git -C "$FOUNT_DIR" pull
+
+if ! command -v deno &> /dev/null; then
+	curl -fsSL https://deno.land/install.sh | sh
+fi
+
 if [ ! -d "$FOUNT_DIR/node_modules" ]; then
-	deno install --allow-scripts --allow-all "--entrypoint=$FOUNT_DIR/src/server/index.mjs" --node-modules-dir=auto
+	deno install --allow-scripts --allow-all --node-modules-dir=auto --entrypoint "$FOUNT_DIR/src/server/index.mjs"
 fi
 
 deno run --allow-scripts --allow-all "$FOUNT_DIR/src/server/index.mjs" $@
