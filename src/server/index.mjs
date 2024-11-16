c25e3ea881856c52075b1d0ab59d5fccc144d14b
steve02081504 <steve02081504@EDEN.fukwold>
2024-11-16 17:50:15 +0800

    update

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -1,10 +1,22 @@
+import process from 'node:process'
 import { init } from "./server.mjs"
-import { loadShell } from "./shell_manager.mjs"
+import { IPCManager } from "./ipc_manager.mjs"
 
-await init()
 let args = process.argv.slice(2)
 
+const isFirstInstance = await init()
+
 if (args.length) {
-	let shell = await loadShell(args[0], args[1])
-	await shell.ArgumentsHandler(args[0], args.slice(2))
+	let username = args[0]
+	let shellname = args[1]
+	args = args.slice(2)
+
+	try {
+		await IPCManager.sendCommand('shell', { username, shellname, args })
+	} catch (err) {
+		console.error('发送命令失败：', err)
+		process.exit(1)
+	}
 }
+
+if (!isFirstInstance) process.exit(0)
