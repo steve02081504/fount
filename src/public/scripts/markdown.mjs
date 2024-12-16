ddbfb2467bc88fe8aa15cf34e779d83234853b9e
Gaoyao Massimo Hu <ghzero97@gmail.com>
2024-12-16 17:35:04 +0100

    feat:add remark-gfm render table

--- src/public/scripts/markdown.mjs
+++ src/public/scripts/markdown.mjs
@@ -6,18 +6,25 @@ import remarkRehype from 'https://esm.sh/remark-rehype@11?bundle'
 import remarkMath from 'https://esm.sh/remark-math@6?bundle'
 import { unified } from 'https://esm.sh/unified@11?bundle'
 import rehypeShiki from 'https://esm.sh/@shikijs/rehype@1?bundle'
+import remarkGfm from 'https://esm.sh/remark-gfm@4?bundle'
 
 export async function renderMarkdown(markdown) {
+	console.log('renderMarkdown', markdown)
 	const file = await unified()
 		.use(remarkParse)
 		.use(remarkMath)
-		.use(remarkRehype).use(rehypeShiki, {
+		.use(remarkGfm)
+		.use(remarkRehype)
+		.use(rehypeShiki, {
 			// or `theme` for a single theme
 			themes: {
 				light: 'vitesse-light',
 				dark: 'vitesse-dark',
 			},
 			inline: 'tailing-curly-colon', // or other options
-		}).use(rehypeKatex).use(rehypeStringify).process(markdown)
+		})
+		.use(rehypeKatex)
+		.use(rehypeStringify)
+		.process(markdown)
 	return String(file)
 }
