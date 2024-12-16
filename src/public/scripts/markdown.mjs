4896c0538e54961608ffa1c67175f75b0a151b69
steve02081504 <steve02081504@EDEN.fukwold>
2024-12-17 01:14:04 +0800

    make pr more fount

--- src/public/scripts/markdown.mjs
+++ src/public/scripts/markdown.mjs
@@ -1,30 +1,37 @@
+import rehypeKatex from 'https://esm.sh/rehype-katex'
+import rehypeStringify from 'https://esm.sh/rehype-stringify'
+import remarkParse from 'https://esm.sh/remark-parse'
+import remarkRehype from 'https://esm.sh/remark-rehype'
+import remarkMath from 'https://esm.sh/remark-math'
+import { unified } from 'https://esm.sh/unified'
+import rehypeShiki from 'https://esm.sh/@shikijs/rehype'
+import remarkGfm from 'https://esm.sh/remark-gfm'
+import remarkBreaks from 'https://esm.sh/remark-breaks'
 
-import rehypeKatex from 'https://esm.sh/rehype-katex@7?bundle'
-import rehypeStringify from 'https://esm.sh/rehype-stringify@10?bundle'
-import remarkParse from 'https://esm.sh/remark-parse@11?bundle'
-import remarkRehype from 'https://esm.sh/remark-rehype@11?bundle'
-import remarkMath from 'https://esm.sh/remark-math@6?bundle'
-import { unified } from 'https://esm.sh/unified@11?bundle'
-import rehypeShiki from 'https://esm.sh/@shikijs/rehype@1?bundle'
-import remarkGfm from 'https://esm.sh/remark-gfm@4?bundle'
+const convertor = unified()
+	.use(remarkParse)
+	.use(remarkBreaks)
+	.use(remarkMath)
+	.use(remarkRehype, {
+		allowDangerousHtml: true,
+	})
+	.use(remarkGfm)
+	.use(rehypeShiki, {
+		// or `theme` for a single theme
+		themes: {
+			light: 'vitesse-light',
+			dark: 'vitesse-dark',
+		},
+		inline: 'tailing-curly-colon', // or other options
+	})
+	.use(rehypeKatex)
+	.use(rehypeStringify, {
+		allowDangerousCharacters: true,
+		allowDangerousHtml: true,
+		tightBreaks: true,
+	})
 
 export async function renderMarkdown(markdown) {
-	console.log('renderMarkdown', markdown)
-	const file = await unified()
-		.use(remarkParse)
-		.use(remarkMath)
-		.use(remarkGfm)
-		.use(remarkRehype)
-		.use(rehypeShiki, {
-			// or `theme` for a single theme
-			themes: {
-				light: 'vitesse-light',
-				dark: 'vitesse-dark',
-			},
-			inline: 'tailing-curly-colon', // or other options
-		})
-		.use(rehypeKatex)
-		.use(rehypeStringify)
-		.process(markdown)
+	const file = await convertor.process(markdown)
 	return String(file)
 }
