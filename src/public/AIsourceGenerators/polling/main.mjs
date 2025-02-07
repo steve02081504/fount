5e67e889909977906fee9fe0947b2fc0928fbbd5
steve02081504 <steve02081504@EDEN.fukwold>
2025-02-07 22:47:10 +0800

    add polling

---
+++ src/public/AIsourceGenerators/polling/main.mjs
@@ -0,0 +1,60 @@
+/** @typedef {import('../../../decl/AIsource.ts').AIsource_t} AIsource_t */
+/** @typedef {import('../../../decl/prompt_struct.ts').prompt_struct_t} prompt_struct_t */
+
+import { loadAIsource } from '../../../server/managers/AIsources_manager.mjs'
+
+export default {
+	GetSource: async (config, { username }) => {
+		let index = 0
+		const sources = await Promise.all(config.sources.map(source => loadAIsource(username, source)))
+		/** @type {AIsource_t} */
+		const result = {
+			type: 'text-chat',
+			info: {
+				'': {
+					avatar: '',
+					name: config.name,
+					provider: config.provider || 'unknown',
+					description: 'polling',
+					description_markdown: 'polling',
+					version: '0.0.0',
+					author: 'steve02081504',
+					homepage: '',
+					tags: ['polling'],
+				}
+			},
+			is_paid: false,
+			extension: {},
+
+			Unload: () => { },
+			Call: async (prompt) => {
+				while (true) try {
+					index++
+					index %= config.sources.length
+					return await sources[index].Call(prompt)
+				}
+				catch (e) {
+					console.error(e)
+				}
+			},
+			StructCall: async (/** @type {prompt_struct_t} */ prompt_struct) => {
+				while (true) try {
+					index++
+					index %= config.sources.length
+					return await sources[index].StructCall(prompt_struct)
+				}
+				catch (e) {
+					console.error(e)
+				}
+			},
+			Tokenizer: {
+				free: () => 0,
+				encode: (prompt) => prompt,
+				decode: (tokens) => tokens,
+				decode_single: (token) => token,
+				get_token_count: (prompt) => prompt.length
+			}
+		}
+		return result
+	}
+}
