832ae3beb36edc902e188c2a0f5940aa578aceb3
steve02081504 <steve02081504@EDEN.fukwold>
2025-02-14 01:54:24 +0800

    update

---
+++ src/server/jobs.mjs
@@ -0,0 +1,28 @@
+import { loadShell } from './managers/shell_manager.mjs'
+import { getUserByUsername } from './auth.mjs'
+import { getAllUserNames } from './auth.mjs'
+import { save_config } from './server.mjs'
+
+export function StartJob(username, shellname, uid, data = null) {
+	const jobs = getUserByUsername(username).jobs ??= {}
+	jobs[shellname] ??= {}
+	jobs[shellname][uid] = data
+	save_config()
+}
+export function EndJob(username, shellname, uid) {
+	delete getUserByUsername(username).jobs[shellname][uid]
+	save_config()
+}
+export async function ReStartJobs() {
+	const users = getAllUserNames()
+	for (const user of users) {
+		const jobs = getUserByUsername(user).jobs ??= {}
+		for (const shellname in jobs)
+			for (const uid in jobs[shellname]) {
+				console.log(`Restarting ${user}'s ${shellname} Job: ${uid}`)
+				const shell = await loadShell(user, shellname)
+				await shell.ReStartJob(user, jobs[shellname][uid] ?? uid, uid)
+			}
+
+	}
+}
