d8ca919fd74b818701530b2aab80f285cf5ede06
steve02081504 <steve02081504@EDEN.fukwold>
2025-02-19 10:56:21 +0800

    add freeuse

---
+++ src/public/AIsourceGenerators/freeuse/main.mjs
@@ -0,0 +1,43 @@
+/** @typedef {import('../../../decl/AIsource.ts').AIsource_t} AIsource_t */
+/** @typedef {import('../../../decl/prompt_struct.ts').prompt_struct_t} prompt_struct_t */
+
+export default {
+	GetSource: async (config) => {
+		/** @type {AIsource_t} */
+		const result = {
+			type: 'text-chat',
+			info: {
+				'': {
+					avatar: '',
+					name: config.name || config.model || 'freeuse',
+					provider: 'freeuse',
+					description: 'freeuse',
+					description_markdown: 'freeuse',
+					version: '0.0.0',
+					author: 'steve02081504',
+					homepage: '',
+					tags: ['free'],
+				}
+			},
+			is_paid: false,
+			extension: {},
+
+			Unload: () => { },
+			Call: async (prompt) => {
+				return '好的，没问题。'
+			},
+			StructCall: async (/** @type {prompt_struct_t} */ prompt_struct) => {
+				return '好的，没问题。'
+			},
+			Tokenizer: {
+				free: () => 0,
+				encode: (prompt) => prompt,
+				decode: (tokens) => tokens,
+				decode_single: (token) => token,
+				get_token_count: (prompt) => model.countTokens(prompt)
+			}
+		}
+
+		return result
+	}
+}
