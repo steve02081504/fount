f428f30b7c14f88f10c914ee46b80c9a9b70ad08
steve02081504 <steve02081504@EDEN.fukwold>
2025-02-23 12:52:06 +0800

    update

--- src/public/shells/shellassist/index.mjs
+++ src/public/shells/shellassist/index.mjs
@@ -4,6 +4,7 @@ applyTheme()
 import { Terminal } from 'https://esm.run/xterm'
 import { WebLinksAddon } from 'https://esm.run/@xterm/addon-web-links'
 import { ClipboardAddon } from 'https://esm.run/@xterm/addon-clipboard'
+import { FitAddon } from 'https://esm.run/@xterm/addon-fit'
 import { initTranslations, geti18n } from '../../scripts/i18n.mjs'
 
 const terminal = new Terminal({
@@ -18,10 +19,16 @@ const terminal = new Terminal({
 	cursorBlink: true
 })
 
+let fiter = new FitAddon()
+terminal.loadAddon(fiter)
 terminal.loadAddon(new WebLinksAddon())
-
 terminal.loadAddon(new ClipboardAddon())
+
 terminal.open(document.getElementById('terminal'))
+fiter.fit()
+window.addEventListener('resize', () => {
+	fiter.fit();
+});
 terminal.element.addEventListener('contextmenu', async (event) => {
 	event.preventDefault()
 	const selectedText = terminal.getSelection()
@@ -33,7 +40,7 @@ terminal.element.addEventListener('contextmenu', async (event) => {
 		terminal.paste(await navigator.clipboard.readText())
 })
 
-await initTranslations('terminal_assistant') // Initialize translations with pageid 'terminal_assistant'
+await initTranslations('terminal_assistant')
 
 terminal.writeln(geti18n('terminal_assistant.initialMessage'))
 terminal.writeln(`\x1b]8;;https://github.com/steve02081504/fount-pwsh\x07${geti18n('terminal_assistant.initialMessageLink')}\x1b]8;;\x07`)
