00df08fada55d67a0de2b0f5bb6dbff17ce73501
steve02081504 <steve02081504@EDEN.fukwold>
2025-02-23 17:30:27 +0800

    fixes

--- src/public/ImportHanlders/SillyTavern/engine/regex.mjs
+++ src/public/ImportHanlders/SillyTavern/engine/regex.mjs
@@ -9,8 +9,12 @@ import { parseRegexFromString } from './tools.mjs'
 export function runRegex(charData, regex_type, text) {
 	if (charData?.extensions?.regex_scripts) {
 		const WI_regex_scripts = charData.extensions.regex_scripts.filter(e => e.placement.includes(regex_type))
-		for (const script of WI_regex_scripts) script.findRegex = parseRegexFromString(String(script.findRegex))
+		for (const script of WI_regex_scripts) script.findRegexObject = parseRegexFromString(String(script.findRegex)) || new RegExp(script.findRegex)
 		for (const script of WI_regex_scripts)
-			text = text.replace(script.findRegex, script.replaceString)
+			text = text.replace(script.findRegexObject, script.replaceString)
+		for (const script of WI_regex_scripts)
+			delete script.findRegexObject
 	}
+
+	return text
 }
