123ac0f5331a160dd4a49c4fea3f90784b9c1ef9
steve02081504 <steve02081504@EDEN.fukwold>
2025-02-28 03:17:37 +0800

    add rpc support

---
+++ src/scripts/discordrpc.mjs
@@ -0,0 +1,60 @@
+import DiscordRPC from 'npm:fixed-discord-rpc'
+import { in_docker, in_termux } from './env.mjs'
+
+// Set this to your Client ID.
+const clientId = '1344722070323335259'
+
+let rpc
+
+const FountStartTimestamp = new Date()
+let _activity = {
+
+}
+function _setActivity() {
+	if (!rpc) return
+	rpc.setActivity({
+		startTimestamp: FountStartTimestamp,
+		..._activity
+	})
+}
+
+export function StartRPC(
+	clientId = '1344722070323335259',
+	activity = {
+		details: 'fountting',
+		state: Array(Math.floor(Math.random() * 7)).fill('fo-').join('') + 'fount!',
+		startTimestamp: undefined,
+		largeImageKey: 'icon',
+		largeImageText: 'github.com/steve02081504/fount',
+		smallImageKey: '',
+		smallImageText: '',
+		instance: false,
+	}
+) {
+	if (process.platform === 'win32') return // https://github.com/denoland/deno/issues/28332
+
+	if (in_docker || in_termux) return
+
+	StopRPC()
+	rpc ??= new DiscordRPC.Client({ transport: 'ipc' })
+
+	SetActivity(activity)
+
+	rpc.on('ready', () => {
+		_setActivity()
+
+		// activity can only be set every 15 seconds
+		setInterval(() => { _setActivity() }, 15e3)
+	})
+
+	rpc.login({ clientId }).catch(console.error)
+}
+
+export function SetActivity(activity) {
+	_activity = activity
+}
+
+export function StopRPC() {
+	if (!rpc) return
+	rpc.destroy()
+}
