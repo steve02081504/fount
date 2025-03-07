791c492f4f8ca1b15ca22dfcd4e1c0d349675f4c
steve02081504 <steve02081504@EDEN.fukwold>
2025-03-08 00:33:10 +0800

    fix

--- src/public/scripts/template.mjs
+++ src/public/scripts/template.mjs
@@ -52,7 +52,7 @@ export function createDocumentFragmentFromHtmlString(htmlString) {
 export function createDOMFromHtmlString(htmlString) {
 	const div = document.createElement('div')
 	div.appendChild(createDocumentFragmentFromHtmlString(htmlString))
-	return div.children[0]
+	return div
 }
 
 export async function renderTemplate(template, data) {
