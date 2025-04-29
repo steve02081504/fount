79ff9a308248341d13d85502c844386c5c2efd71
steve02081504 <steve02081504@EDEN.fukwold>
2025-04-29 16:47:40 +0800

    update

--- src/decl/WorldAPI.ts
+++ src/decl/WorldAPI.ts
@@ -1,5 +1,4 @@
 import { chatReply_t, chatReplyRequest_t } from "../public/shells/chat/decl/chatLog.ts";
-import { AIsource_t } from "./AIsource.ts";
 import { locale_t } from "./basedefs.ts";
 import { chatLogEntry_t, prompt_struct_t, single_part_prompt_t } from "./prompt_struct.ts";
 
@@ -40,7 +39,11 @@ export class WorldAPI_t {
 			GetGroupGreeting?: (arg: chatReplyRequest_t, index: number) => Promise<chatReply_t | null>
 			GetPrompt?: (arg: chatReplyRequest_t, prompt_struct: prompt_struct_t, detail_level: number) => Promise<single_part_prompt_t>;
 			GetChatLogForCharname?: (arg: chatReplyRequest_t, charname: string) => Promise<chatLogEntry_t[]>
-			AddChatLogEntry?: (arg: chatReplyRequest_t, entry: chatLogEntry_t) => void
+			AddChatLogEntry?: (arg: chatReplyRequest_t, entry: chatLogEntry_t) => Promise<void>
+			AfterAddChatLogEntry?: (arg: chatReplyRequest_t, freq_data: {
+				charname: null;
+				frequency: number;
+			}[]) => Promise<void>
 			GetCharReply?: (arg: chatReplyRequest_t, charname: string) => Promise<chatReply_t | null>
 			MessageEdit?: (arg: {
 				index: number
