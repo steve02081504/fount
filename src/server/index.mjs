98717988e07aae117aa06bd6b6043b41fbb54d53
steve02081504 <steve02081504@EDEN.fukwold>
2025-05-05 18:02:43 +0800

    Update index.mjs

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -1,4 +1,4 @@
-import * as Sentry from 'npm:@sentry/deno'
+import * as Sentry from '@sentry/bun'
 Sentry.init({
 	dsn: 'https://17e29e61e45e4da826ba5552a734781d@o4509258848403456.ingest.de.sentry.io/4509258936090704',
 })
