daea99ffa84312923278273c71e49065518d1aa6
steve02081504 <steve02081504@EDEN.fukwold>
2025-05-25 16:27:11 +0800

    Update main.mjs

--- src/public/shells/shellassist/main.mjs
+++ src/public/shells/shellassist/main.mjs
@@ -1,3 +1,5 @@
+import { LoadChar } from '../../../server/managers/char_manager.mjs'
+import { GetDefaultShellAssistInterface } from './src/server/default_interface/main.mjs'
 import { setEndpoints } from './src/server/endpoints.mjs'
 
 export default {
@@ -18,5 +20,26 @@ export default {
 	},
 	Unload: () => {},
 	interfaces: {
+		invokes: {
+			IPCInvokeHandler: async (username, data) => {
+				const char = await LoadChar(username, data.charname)
+				char.interfaces.shellassist ??= await GetDefaultShellAssistInterface(char, data.charname)
+				const result = await char.interfaces.shellassist.Assist({
+					...data,
+					username,
+					UserCharname: data.UserCharname || username,
+					chat_scoped_char_memory: data.chat_scoped_char_memorys[data.charname] || {},
+					chat_scoped_char_memorys: undefined
+				})
+				return {
+					...result,
+					chat_scoped_char_memorys: {
+						...data.chat_scoped_char_memorys,
+						[data.charname]: result.chat_scoped_char_memory
+					},
+					chat_scoped_char_memory: undefined
+				}
+			}
+		}
 	}
 }
