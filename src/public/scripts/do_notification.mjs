342be318b4db774e6ee0b1f4698e12f6235cc55b
steve02081504 <steve02081504@EDEN.fukwold>
2025-06-11 14:12:48 +0800

    dropdown

--- src/public/scripts/do_notification.mjs
+++ src/public/scripts/do_notification.mjs
@@ -1,5 +1,5 @@
 export async function do_notification(title, options) {
-	if (Notification?.permission != 'granted') return
+	if (window.Notification?.permission != 'granted') return
 	try { return new Notification(title, options) }
 	catch (_) {
 		return navigator?.serviceWorker?.ready?.then?.((registration) => {
