230fdf502ed45aefed9f51624a1b90aff882e321
steve02081504 <steve02081504@EDEN.fukwold>
2025-06-20 14:19:19 +0800

    update

--- src/public/AIsourceGenerators/cohere/main.mjs
+++ src/public/AIsourceGenerators/cohere/main.mjs
@@ -99,6 +99,8 @@ ${chatLogEntry.content}
 						).join('|')
 					})\\s*<\\/sender>\\s*<content>`
 				)).pop().split(/<\/content>\s*<\/message/).shift()
+			if (text.match(/<\/content>\s*<\/message[^>]*>\s*$/))
+				text = text.split(/<\/content>\s*<\/message[^>]*>\s*$/).shift()
 
 			const removeduplicate = [...new Set(text.split('\n'))].join('\n')
 			if (removeduplicate.length / text.length < 0.3)
