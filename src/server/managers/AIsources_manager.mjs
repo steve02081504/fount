187353f8b51839e0b296aee0696342a56c99c750
steve02081504 <steve02081504@EDEN.fukwold>
2025-06-27 13:13:05 +0800

    Deprecate AIsources_manager.mjs with warning wrappers
    Replaces direct re-export with explicit deprecated function wrappers that log a warning and stack trace, guiding users to use AIsource_manager.mjs instead. This change prepares for the removal of AIsources_manager.mjs by alerting developers of its deprecation.

--- src/server/managers/AIsources_manager.mjs
+++ src/server/managers/AIsources_manager.mjs
@@ -1,2 +1,88 @@
 // 旧版本兼容文件
-export * from './AIsource_manager.mjs'
+const funcs = await import('./AIsource_manager.mjs')
+const warnText = `
+=========================================================================
+!!!You are using a deprecated file and this file will be removed in few days!!!
+!!!use \`AIsource_manager.mjs\` instead.!!!
+=========================================================================
+`
+
+/**
+ * @deprecated
+ */
+export function loadAIsourceGenerator(...args) {
+	console.warn(warnText)
+	console.trace()
+	const { loadAIsourceGenerator } = funcs
+	return loadAIsourceGenerator(...args)
+}
+
+/**
+ * @deprecated
+ */
+export function unloadAIsourceGenerator(...args) {
+	console.warn(warnText)
+	console.trace()
+	const { unloadAIsourceGenerator } = funcs
+	return unloadAIsourceGenerator(...args)
+}
+
+/**
+ * @deprecated
+ */
+export function loadAIsourceFromConfigData(...args) {
+	console.warn(warnText)
+	console.trace()
+	const { loadAIsourceFromConfigData } = funcs
+	return loadAIsourceFromConfigData(...args)
+}
+
+/**
+ * @deprecated
+ */
+export function loadAIsource(...args) {
+	console.warn(warnText)
+	console.trace()
+	const { loadAIsource } = funcs
+	return loadAIsource(...args)
+}
+
+/**
+ * @deprecated
+ */
+export function loadAIsourceFromNameOrConfigData(...args) {
+	console.warn(warnText)
+	console.trace()
+	const { loadAIsourceFromNameOrConfigData } = funcs
+	return loadAIsourceFromNameOrConfigData(...args)
+}
+
+/**
+ * @deprecated
+ */
+export function unloadAIsource(...args) {
+	console.warn(warnText)
+	console.trace()
+	const { unloadAIsource } = funcs
+	return unloadAIsource(...args)
+}
+
+/**
+ * @deprecated
+ */
+export function isAIsourceLoaded(...args) {
+	console.warn(warnText)
+	console.trace()
+	const { isAIsourceLoaded } = funcs
+	return isAIsourceLoaded(...args)
+}
+
+/**
+ * @deprecated
+ */
+export function reloadAIsource(...args) {
+	console.warn(warnText)
+	console.trace()
+	const { reloadAIsource } = funcs
+	return reloadAIsource(...args)
+}
