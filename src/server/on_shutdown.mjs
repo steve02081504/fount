1ad3f416a67e64f42c097b930bfdb868821d654e
steve02081504 <steve02081504@EDEN.fukwold>
2025-07-01 10:40:00 +0800

    update

--- src/server/on_shutdown.mjs
+++ src/server/on_shutdown.mjs
@@ -1,17 +1,16 @@
 import process from 'node:process'
-import { tray } from './server.mjs'
 
 const shutdowm_functions = []
 export function on_shutdown(func) {
 	shutdowm_functions.unshift(func)
 }
-export async function shutdown(code = 0) {
+async function shutdown(code = 0) {
 	for (const func of shutdowm_functions)
 		await func()
-	if (tray) tray.kill()
 	process.exit(code)
 }
 
 process.on('SIGINT', shutdown)
 process.on('SIGTERM', shutdown)
 process.on('SIGHUP', shutdown)
+process.on('exit', shutdown)
