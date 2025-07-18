3c3d09f19a0b9526186c2fd94beb33c235c5f1bb
steve02081504 <steve02081504@EDEN.fukwold>
2025-07-18 22:53:58 +0800

    feat: improve protocol handler
    Separated JavaScript from HTML in the protocol handler page.
    
    The new protocol handler logic is as follows:
    1. Ping the stored `hostUrl` from local storage.
    2. If the ping is successful, redirect to the `hostUrl`.
    3. If the ping fails, check if the `hostUrl` is a local address.
    4. If it is a local address, try to open the protocol link directly.
    5. Otherwise, get a new `hostUrl` and then redirect.

---
+++ .github/pages/protocol/index.mjs
@@ -0,0 +1,55 @@
+/* global urlParams */
+import '../base.mjs'
+import * as Sentry from 'https://esm.run/@sentry/browser'
+import { getFountHostUrl } from '../scripts/fountHostGetter.mjs'
+import { initTranslations, geti18n } from '../scripts/i18n.mjs'
+
+const fountProtocolUrl = urlParams.get('url')
+
+/**
+ * @param {string} hostUrl
+ */
+async function ping(hostUrl) {
+	const controller = new AbortController()
+	const timeout = setTimeout(() => controller.abort(), 1000)
+	const res = await fetch(new URL('/api/ping', hostUrl), {
+		signal: controller.signal,
+	}).catch(() => 0)
+	clearTimeout(timeout)
+	return res?.ok
+}
+
+function useUrlProtocol(hostUrl) {
+	if (fountProtocolUrl) {
+		const redirectUrl = new URL('/protocolhandler', hostUrl)
+		redirectUrl.searchParams.set('url', fountProtocolUrl)
+		redirectUrl.searchParams.set('from', 'jumppage')
+		window.location.href = redirectUrl.href
+	} else {
+		window.location.href = new URL('/shells/home', hostUrl).href
+	}
+}
+
+async function main() {
+	await initTranslations('protocolhandler')
+	const hostUrl = urlParams.get('hostUrl') ?? localStorage.getItem('fountHostUrl')
+	if (hostUrl) {
+		const isOnline = await ping(hostUrl)
+		if (isOnline) return useUrlProtocol(hostUrl)
+
+		const url = new URL(hostUrl)
+		if (fountProtocolUrl && ['localhost', '127.0.0.1'].includes(url.hostname))
+			return window.open(fountProtocolUrl, '_self')
+	}
+
+	const newHostUrl = await getFountHostUrl()
+	if (newHostUrl) return useUrlProtocol(newHostUrl)
+	alert(geti18n('protocolhandler.fountNotFound'))
+	window.location.href = 'https://github.com/steve02081504/fount'
+}
+
+main().catch(e => {
+	Sentry.captureException(e)
+	alert(geti18n('protocolhandler.unknownError') + e.message)
+	window.location.href = 'https://github.com/steve02081504/fount'
+})
