9eabf8734740f169c354fa32ff9909f7be2b910b
steve02081504 <steve02081504@EDEN.fukwold>
2025-08-01 13:31:53 +0800

    easynew

---
+++ src/public/shells/easynew/main.mjs
@@ -0,0 +1,20 @@
+import { setEndpoints } from './src/server/main.mjs'
+
+/** @type {import('../../../../decl/shellAPI.ts').ShellAPI_t} */
+export default {
+	info: {
+		'': {
+			name: 'EasyNew',
+			description: 'Easily create new parts from templates.',
+			version: '1.0.0',
+			author: 'steve02081504',
+			tags: ['tool', 'creator'],
+		},
+	},
+
+	Load: async ({ router }) => {
+		setEndpoints(router)
+	},
+
+	Unload: () => { },
+}
