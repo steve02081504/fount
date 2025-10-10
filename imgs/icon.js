7ad07a2e647bb5b68d6a547b34b82f6ddba2a530
Canmi <9997200@qq.com>
2025-10-11 01:39:44 +0800

    -18 byte

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,2 +1,2 @@
-c=[[]];for(s of` 9 9@2:1,2; 8 8:2@2:1,2; 7 7@2:2@2:1,3; 9@3 2@2:2@2:1,8; 8 8:2 2:1,1; 5@8@8,4`.split`;`)for([e,n]=s.split`,`,g=[...o=e.match(/../g).map(([a,b])=>a.repeat(b)),...o.reverse()];n--;c.push(...g.map(x=>`color:#0${x<'@'?'ff':11}`)))c[0]+='%c'+g.join`%c`+`
-`,c
+c=[[]];for(s of`		!,2;"!,2;"!,3;	"!,8;"!,1;,4`.split`;`)for([e,n]=s.split`,`,g=[...o=[...e].map(x=>" @:"[(k=x.charCodeAt())>>4].repeat(k&15)),...o.reverse()];n--;g.map(x=>c.push(`color:#0${x<'@'?'ff':11}`)),c[0]+=`%c${g.join`%c`}
+`);c
