fb61052a7c49160dc928eb093d835ff80c33cbfd
尤桉哲 <726925273@qq.com>
2025-10-11 00:56:38 +0800

    Loto2 348->268

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,2 +1,2 @@
-c=[[]];for(s of` 18@2:1,2; 16:2@2:1,2; 14@2:2@2:1,3; 9@3 2@2:2@2:1,8; 16:2 2:1,1; 5@16,4`.split`;`)for([e,n]=s.split`,`,g=[...o=e.match(/.\d+/g).map(m=>m[0].repeat(m.slice(1))),...o.reverse()];n--;c.push(...g.map(x=>`color:#0${x<'@'?'ff':11}`)))c[0]+='%c'+g.join`%c`+`
+c=[[]];for(s of` 9 9@2:1,2; 8 8:2@2:1,2; 7 7@2:2@2:1,3; 9@3 2@2:2@2:1,8; 8 8:2 2:1,1; 5@8@8,4`.split`;`)for([e,n]=s.split`,`,g=[...o=e.match(/../g).map(([a,b])=>a.repeat(b)),...o.reverse()];n--;c.push(...g.map(x=>`color:#0${x<'@'?'ff':11}`)))c[0]+='%c'+g.join`%c`+`
 `
