77584302e13cf50282b66a468276c22ec0668afd
尤桉哲 <726925273@qq.com>
2025-10-11 00:54:24 +0800

    Loto2 348->271

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,2 +1,2 @@
 c=[[]];for(s of` 18@2:1,2; 16:2@2:1,2; 14@2:2@2:1,3; 9@3 2@2:2@2:1,8; 16:2 2:1,1; 5@16,4`.split`;`)for([e,n]=s.split`,`,g=[...o=e.match(/.\d+/g).map(m=>m[0].repeat(m.slice(1))),...o.reverse()];n--;c.push(...g.map(x=>`color:#0${x<'@'?'ff':11}`)))c[0]+='%c'+g.join`%c`+`
-`,c
+`
