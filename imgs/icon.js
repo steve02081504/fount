22f29c124f2245d75ff8002387d51cf979a3441a
KC <48410934+orangelckc@users.noreply.github.com>
2025-10-11 00:18:13 +0200

    Update icon.js-204

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,4 +1,4 @@
 c=[h=r='']
-for(k of`\t\t\x12!2\b\b"\x12!2\x07\x07\x12"\x12!3\t\x13\x02\x12"\x12!8\b\b"\x02!1\x05\x18\x184`)for(p of+k?(h+r+`
+for(k of`		!2"!2"!3	"!8"!14`)for(p of+k?(h+r+`
 `).repeat(k):(h+=s=' @:'[(k=k.charCodeAt())>>4].repeat(k&15),r=s+r,''))c[0]+='%c'+p,c.push('color:#0'+(p>'?'?11:'ff')),h=r=''
 c
