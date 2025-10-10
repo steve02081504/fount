d3679e9603c8d5d7703b42e5325445fda2a0f479
SevenRu <113108504+SevenMrs@users.noreply.github.com>
2025-10-10 22:38:42 +0800

    269

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,2 +1,2 @@
-c=[''];` 9 9@2:1,2; 8 8:2@2:1,2; 7 7@2:2@2:1,3; 9@3 2@2:2@2:1,8; 8 8:2 2:1,1; 5@8@8,4`.split`;`.map(s=>{[e,n]=s.split`,`,g=[...o=e.match(/../g).map(c=>c[0].repeat(c[1])),...o.reverse()];for(;n--;c.push(...g.slice(1).map(c=>'color:#0'+(c[0]==':'?'ff':'00'))))c[0]+=g.join`%c`+`
-`}),c
+c=[[]];for(s of` 9 9@2:1,2; 8 8:2@2:1,2; 7 7@2:2@2:1,3; 9@3 2@2:2@2:1,8; 8 8:2 2:1,1; 5@8@8,4`.split`;`)for([e,n]=s.split`,`,g=[...o=e.match(/../g).map(([a,b])=>a.repeat(b)),...o.reverse()];n--;c.push(...g.map(x=>`color:#0${x<'@'?'ff':11}`)))c[0]+='%c'+g.join`%c`+`
+`
