162e22ad15f1ef5eaf290bbcb84ed0eae51a9230
ring <54168736+alex0125z@users.noreply.github.com>
2025-10-11 14:57:42 +0800

    ?

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,4 +1,3 @@
-c=[h=r='']
-for(k of`		!2"!2"!3	"!8"!14`)for(p of+k?(h+r+`
-`).repeat(k):(h+=s=' @:'[(k=k.charCodeAt())>>4].repeat(k&15),r=s+r,''))c[0]+='%c'+p,c.push('color:#0'+(p>'?'?11:'ff')),h=r=''
-c
+c=[q=h=r='']
+for(k of`"A2B"A2"B"A3	#"B"A8BA104`)for(p of(h+r+`
+`).repeat(k)||(h+=s=' @:'[(k=k.charCodeAt())>>5].repeat(k&31),r=s+r,q))c[0]+='%c'+p,c.push('color:#0'+(p>';'?11:'ff')),h=r=q
