ca11c53a985aada8452beaa1423a2ff5f30e9e5a
ring <54168736+alex0125z@users.noreply.github.com>
2025-10-11 10:59:44 +0800

    变异!
    太坏了,只能变异了.

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,4 +1,4 @@
-c=[h=r='']
+c=[h=r]
 for(k of`		!2"!2"!3	"!8"!14`)for(p of+k?(h+r+`
 `).repeat(k):(h+=s=' @:'[(k=k.charCodeAt())>>4].repeat(k&15),r=s+r,''))c[0]+='%c'+p,c.push('color:#0'+(p>'?'?11:'ff')),h=r=''
 c
