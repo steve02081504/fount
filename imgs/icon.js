205cd878408e7974073e879eb5d25534b45f9f99
尤桉哲 <726925273@qq.com>
2025-10-11 14:44:17 +0800

    Loto2 348->197

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,3 +1,3 @@
 c=[h=r='']
-for(k of`"A2B"A2"B"A3\t#"B"A8BA104`)for(p of+k?(h+r+`
+for(k of`"A2B"A2"B"A3	#"B"A8BA104`)for(p of+k?(h+r+`
 `).repeat(k):(h+=s=' @:'[(k=k.charCodeAt())>>5].repeat(k&31),r=s+r,''))c[0]+='%c'+p,c.push('color:#0'+(p>'?'?11:'ff')),h=r=''
