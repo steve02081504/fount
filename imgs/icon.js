60232493b997187f2dcbe71fe8716e359fc8d0c4
ring <54168736+alex0125z@users.noreply.github.com>
2025-10-11 08:36:54 +0800

    -1
    因垂丝汀~

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,5 +1,4 @@
 c=[h=r='']
-for(k of`		!2"!2"!3	"!8"!14`)if(+k){for(;k--;)for(p of h+r+`
-`)c[0]+='%c'+p,c.push(`color:#0${p>'?'?11:'ff'}`);h=r=''}else
-h+=s=' @:'[(k=k.charCodeAt())>>4].repeat(k&15),r=s+r
+for(k of`		!2"!2"!3	"!8"!14`)for(p of+k?(h+r+`
+`).repeat(k):(h+=s=' @:'[(k=k.charCodeAt())>>4].repeat(k&15),r=s+r,''))c[0]+='%c'+p,c.push('color:#0'+(p>'?'?11:'ff')),h=r=''
 c
