208fd1bf1950d3f4142c643681b346cf36a5d44c
尤桉哲 <726925273@qq.com>
2025-10-11 14:09:26 +0800

    Loto2 348->198

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,3 +1,3 @@
 c=[h=r='']
-for(k of`B2B2BB3	CBB81P4`)for(p of+k?(h+r+`
-`).repeat(k):(h+=s=' @:'[(k=k.charCodeAt())>>6].repeat(k&63),r=s+r,''))c[0]+='%c'+p,c.push('color:#0'+(p>'?'?11:'ff')),h=r=''
+for(k of`"A2B"A2"B"A3\t#"B"A8BA104`)for(p of+k?(h+r+`
+`).repeat(k):(h+=s=' @:'[(k=k.charCodeAt())>>5].repeat(k&31),r=s+r,''))c[0]+='%c'+p,c.push('color:#0'+(p>'?'?11:'ff')),h=r=''
