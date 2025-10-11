bcdf65a5fe60d85c7396c71ffa5b52c8c3015b9c
chenxu <lolicon@papapoi.com>
2025-10-11 17:32:13 +0800

    update: 为 https://github.com/steve02081504/fount/pull/138/commits/162e22ad15f1ef5eaf290bbcb84ed0eae51a9230 也按这个方式缩减2字节

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,9 +1,5 @@
-c=[h=r='']
-for(k of`HH
-2@@
-2p
-3H
-8@@
-1(AA4`)for(p of+k?(h+r+`
-`).repeat(k):(k=k.charCodeAt(),h+=s=' @:'[k&3].repeat(k/8),r=s+r,''))c[0]+='%c'+p,c.push('color:#0'+(p>'?'?11:'ff')),h=r=''
+c=[h=r=``];for(k of`J2B	2:	3&
+	8B	
+1@4`)for(p of(h+r+`
+`).repeat(k)||(k=k.charCodeAt(),h+=s=`@: `[k&3].repeat(k/4),r=s+r,''))c[0]+='%c'+p,c.push('color:#0'+(p>'<'?10:'ff')),h=r=''
 c
