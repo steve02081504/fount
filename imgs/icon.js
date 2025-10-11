4bce111c207a32539e16a3412c492919161673cc
ring <54168736+alex0125z@users.noreply.github.com>
2025-10-11 18:36:37 +0800

    要来几包螺丝钉?

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,4 +1,6 @@
-c=[q=h=r='']
-for(k of`"A2B"A2"B"A3	#"B"A8BA104`)for(p of(h+r+`
-`).repeat(k)||(h+=s=' @:'[(k=k.charCodeAt())>>5].repeat(k&31),r=s+r,q))c[0]+='%c'+p,c.push('color:#0'+(p>';'?11:'ff')),h=r=q
+c=[q=h='']
+for(k of`J2	B2	:3	
+&8
+	B1@4`)for(p of(h+`
+`).repeat(k)||(k=k.charCodeAt(),s='@: '[k&3].repeat(k/4),h=s+h+s,q))c[0]+='%c'+p,c.push('color:#0'+(p>'<'?11:'ff')),h=q
 c
