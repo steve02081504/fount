53e3740b124087c86cdb3ede1a9b6e87bdac0349
steve02081504 <steve02081504@EDEN.fukwold>
2025-11-12 14:27:06 +0800

    move to tailwindcss
    as https://github.com/unocss/unocss/issues/4992 and tailwindcss/browser works fine now, we dont need unocss now.

--- src/public/shells/chat/public/src/ui/messageList.mjs
+++ src/public/shells/chat/public/src/ui/messageList.mjs
@@ -141,7 +141,7 @@ function openModal(src, type) {
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>${title}</title>
-	<script src="https://cdn.jsdelivr.net/npm/@unocss/runtime" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" crossorigin="anonymous"></script>
 	<link href="https://cdn.jsdelivr.net/npm/daisyui/daisyui.css" rel="stylesheet" type="text/css" crossorigin="anonymous" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css" crossorigin="anonymous">
 	<style>
