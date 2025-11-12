bb9e48acb4d3628d588757cc6a57774866790dc4
ring <54168736+alex0125z@users.noreply.github.com>
2025-11-13 02:42:14 +0800

    e?

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,6 +1,3 @@
-c=[q=h='']
-for(k of`J2	B2	:3	
-&8
-	B1@4`)for(p of(h+`
-`).repeat(k)||(k=k.charCodeAt(),s='@: '[k&3].repeat(k/4),h=s+h+s,q))c[0]+='%c'+p,c.push('color:#0'+(p>'<'?11:'ff')),h=q
-c
+s=`,color:#000,color:#0ff`.repeat(280).split`,`
+for(e of[227,1199,18696,4624523,982,9**9-5]){a=e/3,c=``;for(i=21;i--;)c=(d=` :`[(a=a/3|0)%3]||`%c@%c`)+c+d;s[0]+=(c+`
+`).repeat(e%9)}s
