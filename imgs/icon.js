caa764a473edf0ec52753ce9ebcb37f7e81e7ba8
Long0x0 <51022287+Long0x0@users.noreply.github.com>
2025-11-16 00:08:23 +0800

    golf~

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,2 +1,2 @@
-s=`,color:#000,color:#0ff`.repeat`280`.split`,`;for(a of[227,1199,18696,4624523,982,9**9-5]){for(b=a%9,a/=3,c=``,i=21;i--;)a=a/3|0,d=` :`[a%3]||`%c@%c`,c=d+c+d;s[0]+=(c+`
-`).repeat(b)}s
+s=`,color:#000,color:#0ff`.repeat`280`.split`,`;for(a of[25,133,2077,513835,109,9**8-1]){for(c=``,i=21;i--;)d=` :`[a%3|0]||`%c@%c`,c=d+c+d,a/=3;s[0]+=(c+`
+`).repeat(a*20e9%478%9)}s
