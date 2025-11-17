28c13c20e16c7fc8f487884be7b623c5235aa4e4
ring <54168736+alex0125z@users.noreply.github.com>
2025-11-17 15:04:26 +0800

    ¿

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,5 +1,5 @@
-s=',color:#000,color:#0ff'.repeat(280).split`,`
-for(a of[25,133,2077,513835,109,9**8-1]){c=''
+s=',color:#000,color:#0ff'.repeat(280)
+for(a of[9**8-1,109,513835,2077,133,25]){c=''
 for(i=21;i--;a/=3)c=(d=' :'[a%3|0]||'%c@%c')+c+d
-s[0]+=(c+`
-`).repeat(a*2e10%478%9)}s
+s=`${c}
+`.repeat(32e69*a%8.5)+s}s.split`,`
