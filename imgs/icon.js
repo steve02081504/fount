7b9000a84342da9ba17d15c0e041a76b936e8442
Long0x0 <51022287+Long0x0@users.noreply.github.com>
2025-11-18 00:00:25 +0800

    golf~~~

--- imgs/icon.js
+++ imgs/icon.js
@@ -1,2 +1,2 @@
-s=`,color:#000,color:#0ff`.repeat`280`.split`,`;for(a of[25,133,2077,513835,109,9**8-1]){for(c=``,i=21;i--;a/=3)d=` :`[a%3|0]||`%c@%c`,c=d+c+d;s[0]+=(c+`
-`).repeat(a*32e69%8.5)}s
+s=`,color:#000,color:#0ff`.repeat`280`;for(a of[9**8-1,109,513835,2077,133,25]){for(c=``,i=21;i--;a/=3)d=` :`[a%3|0]||`%c@%c`,c=d+c+d;s=(c+`
+`).repeat(792871/a%9)+s}s.split`,`
