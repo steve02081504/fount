7ca8bf1ff38951f42e384aa1cfb0b4322443c9bc
steve02081504 <steve02081504@EDEN.fukwold>
2026-03-02 10:55:56 +0800

    fixes

--- src/workers/icongener.mjs
+++ src/workers/icongener.mjs
@@ -10,10 +10,15 @@ setMain(main)
  * 生成图标
  */
 async function main() {
-	const { render: resvg } = await import('https://deno.land/x/resvg_wasm/mod.ts')
+	const { Resvg } = await import('npm:@resvg/resvg-js')
 	const { default: pngToIco } = await import('npm:png-to-ico')
-	const svg = fs.readFileSync(__dirname + '/imgs/icon.svg', 'utf-8')
-	const favpngbuf = await resvg(svg).then(buffer => Buffer.from(buffer))
+	const svg = fs.readFileSync(__dirname + '/imgs/icon.svg')
+	const favpngbuf = Buffer.from(new Resvg(svg, {
+		fitTo: {
+			mode: 'width',
+			value: 1024,
+		},
+	}).render().asPng())
 	nicerWriteFileSync(__dirname + '/src/public/pages/favicon.png', favpngbuf)
 	const favicobuf = await pngToIco(favpngbuf)
 	nicerWriteFileSync(__dirname + '/src/public/pages/favicon.ico', favicobuf)
