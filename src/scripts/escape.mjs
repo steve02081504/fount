c80f5e22a4741a5490acbfcadd6d3393a84a8c5e
steve02081504 <steve02081504@EDEN.fukwold>
2026-03-17 14:49:51 +0800

    fixes

--- src/scripts/escape.mjs
+++ src/scripts/escape.mjs
@@ -16,24 +16,3 @@ export function escapeRegExp(string) {
 export function unescapeRegExp(string) {
 	return string.replace(/\\(.)/g, '$1')
 }
-
-/**
- * 将字符串中的 Unicode 转义序列替换为相应的字符。
- *
- * @param {string} str - 可能包含 Unicode 转义序列的输入字符串。
- * @returns {string} Unicode 转义序列被替换为实际字符的字符串。
- */
-export function unicodeEscapeToChar(str) {
-	return str.replace(/\\u[\dA-Fa-f]{4}/g, match => String.fromCharCode(parseInt(match.replace('\\u', ''), 16)))
-}
-
-/**
- * 将字符串中的 Unicode 转义序列替换为相应的字符。
- *
- * @param {string} str - 可能包含 Unicode 转义序列的输入字符串。
- * @returns {string} Unicode 转义序列被替换为实际字符的字符串。
- */
-export function unescapeUnicode(str) {
-	if (!(Object(str) instanceof String)) str = str.toString()
-	return str.replace(/\\u([\da-f]{4})/gi, (match, p1) => String.fromCharCode(parseInt(p1, 16)))
-}
