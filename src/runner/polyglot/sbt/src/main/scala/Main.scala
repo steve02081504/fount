2521d90feb7d38bbfb14847b11cc85a8766838c3
steve02081504 <steve02081504@EDEN.fukwold>
2026-03-23 17:03:11 +0800

    polyglot: supports `sbt run`

---
+++ src/runner/polyglot/sbt/src/main/scala/Main.scala
@@ -0,0 +1,13 @@
+object Main {
+	def main(args: Array[String]): Unit = {
+		val isWindows = System.getProperty("os.name").toLowerCase.contains("windows")
+
+		val cmd: Seq[String] =
+			if (isWindows) Seq("cmd.exe", "/c", "run.bat") ++ args
+			else Seq("sh", "run.sh") ++ args
+
+		val exitCode = new ProcessBuilder(cmd: _*).inheritIO().start().waitFor()
+		sys.exit(if (exitCode == 0) 0 else exitCode)
+	}
+}
+
