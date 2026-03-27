e139095cfe0122ec3bd8b173e1e59c23f69191ac
hxx <hxx0215@gmail.com>
2026-03-27 20:50:59 +0000

    [feat] upgrade scala version to modern scala3 and add gitignore items to ignore files produced by sbt

--- src/runner/polyglot/sbt/src/main/scala/Main.scala
+++ src/runner/polyglot/sbt/src/main/scala/Main.scala
@@ -1,40 +1,42 @@
-object Main {
-	def main(args: Array[String]): Unit = {
-		val isWindows = System.getProperty("os.name").toLowerCase.contains("windows")
-
-		val scriptName = if (isWindows) "run.bat" else "run.sh"
-		val scriptDir = {
+import java.io.File
+import java.nio.file.Paths
+import java.nio.file.Files
+@main def main(args: String*): Unit = 
+	object PathLocator
+	val isWindows = System.getProperty("os.name").toLowerCase.contains("windows")
+	val scriptName = if isWindows then
+		"run.bat" 
+	else 
+		"run.sh"
+	val codeSource = PathLocator.getClass.getProtectionDomain.getCodeSource
+	val codePath = Option(codeSource).map(cr => Paths.get(cr.getLocation().toURI()))
+		.getOrElse(Paths.get(".").toAbsolutePath())
+	val startDir = if Files.isRegularFile(codePath) then
+		codePath.getParent
+	else 
+		codePath
+	val maybeDir = Iterator
+		.iterate(startDir)(_.getParent())
+		.takeWhile(_ != null)
+		.take(16)
+		.find(d => Files.exists(d.resolve(scriptName)))
+	val scriptDir = maybeDir.getOrElse(Paths.get(System.getProperty("user.dir")))
+	val finalDir = scriptDir.toFile()
 			// In some runtime environments `getCodeSource` (or `getLocation`) can be null.
 			// Fall back to the current working directory in that case.
-			val codeSource = getClass.getProtectionDomain.getCodeSource
-			val location = if (codeSource != null) codeSource.getLocation else null
-			val codePath =
-				if (location != null) new java.io.File(location.toURI).getAbsoluteFile
-				else new java.io.File(".").getAbsoluteFile
-			val startDir = if (codePath.isFile) codePath.getParentFile else codePath
 
 			// Walk up from class/jar location so we can set the correct working directory
 			// (and avoid relying on the JVM current working directory).
-			val maybeDir = Iterator
-				.iterate(startDir)(_.getParentFile)
-				.take(16)
-				.filter(_ != null)
-				.find(d => new java.io.File(d, scriptName).exists())
 
-			maybeDir.getOrElse(new java.io.File(System.getProperty("user.dir")))
-		}
 
-		val cmd: Seq[String] =
+		val cmd =
 			if (isWindows) Seq("cmd.exe", "/c", "run.bat") ++ args
 			else Seq("sh", "run.sh") ++ args
 
 		val exitCode =
-			new ProcessBuilder(cmd: _*)
-				.directory(scriptDir)
+			new ProcessBuilder(cmd*)
+				.directory(finalDir)
 				.inheritIO()
 				.start()
 				.waitFor()
 		sys.exit(exitCode)
-	}
-}
-
