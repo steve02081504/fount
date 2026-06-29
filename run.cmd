94e70266ce5e4e8f678d9cfc9a11da31e67e47a3
steve02081504 <steve02081504@EDEN.fukwold>
2026-06-29 12:30:05 +0800

    Create run.cmd

---
+++ run.cmd
@@ -0,0 +1,19 @@
+: '"
+@echo off
+goto Batch
+"'
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
+SH_EXEC=$(command -v sh)
+"$SH_EXEC" "$SCRIPT_DIR/run.sh" "$@"
+exit $?
+
+:Batch
+if "%1"=="" (
+	set FOUNT_CLICK=1
+	cmd /c "%~dp0/path/fount.bat" open
+) else (
+	cmd /c "%~dp0/path/fount.bat" %*
+)
+if %ERRORLEVEL% NEQ 0 if %ERRORLEVEL% NEQ 255 pause
+exit /b %ERRORLEVEL%
+@echo on
