5fa4d9af21021efed57dec41ce2b2bcf58b7dc05
steve green <steve_green@qq.com>
2026-08-07 00:23:02 +0800

    fix run.bat (#281)

--- path/src/cmd/background.ps1
+++ path/src/cmd/background.ps1
@@ -3,20 +3,18 @@
 	$env:FOUNT_BACKGROUND = 1
 	handle_docker_passthrough @args
 	$cmdArgs = @($args | Select-Object -Skip 1)
-	if (Test-Path -Path "$FOUNT_DIR/.nobackground") {
-		$windowsTerminalCommand = Get-WTfountCmd @cmdArgs
-		Start-Process -FilePath $windowsTerminalCommand.FilePath -ArgumentList $windowsTerminalCommand.ArgumentList
+	try {
+		if (Test-Path -Path "$FOUNT_DIR/.nobackground") {
+			$windowsTerminalCommand = Get-WTfountCmd @cmdArgs
+			Start-Process -FilePath $windowsTerminalCommand.FilePath -ArgumentList $windowsTerminalCommand.ArgumentList -ErrorAction Stop
+		}
+		else {
+			$pwshExe = (Get-Process -Id $PID).Path
+			Start-Process -FilePath $pwshExe -ArgumentList (Get-FountPs1ArgumentList @cmdArgs) -WindowStyle Hidden -ErrorAction Stop
+		}
 	}
-	else {
-		$pwshExe = (Get-Process -Id $PID).Path
-		$fountScript = "$FOUNT_DIR\path\fount.ps1"
-		$argList = @(
-			'-NoProfile',
-			'-NoLogo',
-			'-ExecutionPolicy', 'Bypass',
-			'-File', $fountScript
-		) + @($cmdArgs)
-		Start-Process -FilePath $pwshExe -ArgumentList $argList -WindowStyle Hidden
+	catch {
+		exit 1
 	}
 	exit 0
 }
