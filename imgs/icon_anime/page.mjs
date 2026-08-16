27742b248bb34a1429929c2012f9979482ce8655
steve02081504 <steve02081504@EDEN.fukwold>
2026-08-16 10:44:39 +0800

    1

--- imgs/icon_anime/page.mjs
+++ imgs/icon_anime/page.mjs
@@ -10,10 +10,10 @@ applyTheme()
 
 /**
  * 在容器上挂终端并绑定 icon IO，同时把 `terminal` / `icon` 挂到 `globalThis`。
- * @param {HTMLElement} [element] 终端容器，默认 `#term`
+ * @param {HTMLElement} [element] 终端容器，默认 `#terminal`
  * @returns {import('https://esm.sh/xterm').Terminal} 终端
  */
-export function attach(element = document.getElementById('term')) {
+export function attach(element = document.getElementById('terminal')) {
 	const terminal = setTerminal(element)
 	icon.setIO(terminal)
 	Object.assign(globalThis, { terminal, icon })
