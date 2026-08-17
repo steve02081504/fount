34d06ae044c3cb3b09cca7cfd97a016522439a82
steve02081504 <steve02081504@EDEN.fukwold>
2026-08-17 11:18:38 +0800

    ignore 130 when exit in runs

--- run.bat
+++ run.bat
@@ -13,6 +13,6 @@ if "%1"=="" (
 ) else (
 	cmd /c "%~dp0/path/fount.bat" %*
 )
-if %ERRORLEVEL% NEQ 0 if %ERRORLEVEL% NEQ 255 pause
+if %ERRORLEVEL% NEQ 0 if %ERRORLEVEL% NEQ 130 if %ERRORLEVEL% NEQ 255 pause
 exit /b %ERRORLEVEL%
 @echo on
