b46484d6fec3e1d15b0f46c3a9c5bcb07d1850a6
steve02081504 <steve02081504@EDEN.fukwold>
2026-08-29 03:53:36 +0800

    more alias

---
+++ path/ほうと.ps1
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env pwsh
+echo " \`" > /dev/null # " | Out-Null <#
+SCRIPT_DIR=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
+exec "$(command -v sh || echo /bin/sh)" "$SCRIPT_DIR/fount" "$@"
+exit $?
+: << '__END_HEREDOC__'
+#>
+. $PSScriptRoot/fount.ps1 @args
+exit $LastExitCode
+function __END_HEREDOC__() {}
+__END_HEREDOC__
