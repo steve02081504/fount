c52030254f42b3c47ed70e6b57e47172a69ce090
Taromati2 <taromati2@outlook.com>
2026-08-29 09:13:32 +0000

    file update~

--- src/decl/locale_data.ts
+++ src/decl/locale_data.ts
@@ -6076,6 +6076,75 @@ export type LocaleData = {
 				'aria-label': string
 			}
 		}
+		markdownRichInput: {
+			toolbar: {
+				'aria-label': string
+			}
+			contextMenu: {
+				'aria-label': string
+			}
+			bold: {
+				dataset: {
+					tip: string
+				}
+				'aria-label': string
+			}
+			italic: {
+				dataset: {
+					tip: string
+				}
+				'aria-label': string
+			}
+			strike: {
+				dataset: {
+					tip: string
+				}
+				'aria-label': string
+			}
+			code: {
+				dataset: {
+					tip: string
+				}
+				'aria-label': string
+			}
+			quote: {
+				dataset: {
+					tip: string
+				}
+				'aria-label': string
+			}
+			link: {
+				dataset: {
+					tip: string
+				}
+				'aria-label': string
+			}
+			mention: {
+				dataset: {
+					tip: string
+				}
+				'aria-label': string
+			}
+			copy: {
+				dataset: {
+					tip: string
+				}
+				'aria-label': string
+			}
+			cut: {
+				dataset: {
+					tip: string
+				}
+				'aria-label': string
+			}
+			paste: {
+				dataset: {
+					tip: string
+				}
+				'aria-label': string
+			}
+			linkUrl: string
+		}
 	}
 	pro_cancel_screen: {
 		title: string
